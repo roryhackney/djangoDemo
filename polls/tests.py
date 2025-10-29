c22d7bc01932fb10a8363ce826d77fb77859478b
roryhackney <roryhackney@gmail.com>
2025-10-28 17:26:31 -0700

    Converts to use generic views, basic tests

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,23 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
+from .models import Question
 
-# Create your tests here.
+
+# Create your tests here
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_not_with_future_question(self):
+        # was_published_recently() should return False for questions with pub_date in the future
+        time = timezone.now() + datetime.timedelta(days=30)
+        q = Question(pub_date=time)
+        self.assertIs(q.was_published_recently(), False)
+
+    def test_was_published_recently_not_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=30)
+        q = Question(pub_date=time)
+        self.assertIs(q.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=1)
+        q = Question(pub_date=time)
+        self.assertIs(q.was_published_recently(), True)
--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,55 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import F
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .models import Choice, Question
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello world. You're at the polls index.")
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s" % question_id)
+# def index(request):
+#     recent = Question.objects.order_by("-pub_date")[:5]
+#     return render(request, "polls/index.html", {"recentPosts": recent})
 
-def results(request, question_id):
-    response = "You're looking at results for question %s"
-    return HttpResponse(response % question_id)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "recentPosts"
+
+    #this sets the model similar to model=Question below
+    def get_queryset(self):
+        #retrieves 5 most recent questions
+        return Question.objects.order_by("-pub_date")[:5]
+
+# def detail(request, question_id):
+#     q = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": q})
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+    #don't need to set context_object_name=question bc django defaults to model.tolowercase()
+
+# def results(request, question_id):
+#     q = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": q})
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        #either nothing was selected or an invalid choice by altering the html
+        #rerender the form with an error message
+        return render(request, "polls/detail.html", {"question": question, "error_message": "Please select a choice."})
+    else:
+        #increase the number of votes in the db
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        #always redirect after successfully processing a post form
+        #this prevents resubmission with the back button
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
